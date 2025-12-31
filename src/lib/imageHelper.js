4327a5b192fb137fa030d4301c9750a4d87853f0
Jssagalaka <jaydeepsagalaka@gmail.com>
2025-12-31 10:46:56 +0530

    Revert "Merge pull request #2 from gabbar360/jaydeep-dev"
    This reverts commit 95d68b79128c2be19e5a5331082f289b46e28dcf, reversing
    changes made to b8f6a21dcfba34fecc22f4d77f33c760025f2806.

--- src/lib/imageHelper.js
+++
@@ -1,9 +0,0 @@
-export const getImageUrl = (url) => {
-  if (!url) return '';
-  // If URL already has http/https, use it directly (WordPress/Cloudinary)
-  if (url.startsWith('http')) {
-    return url;
-  }
-  // Otherwise, return local file path directly
-  return url;
-};
